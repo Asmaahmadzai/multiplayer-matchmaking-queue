ec1c42d5f6ef9b0de75ad6c745274a5f7a6a16d0
Asma Ahmadzai <asmaahmadzai06@gmail.com>
2026-09-08 19:44:55 -0400

    Remove student information and comments from header

--- data_structures.h
+++ data_structures.h
@@ -1,13 +1,6 @@
-/*************************************************
- * Student Name: Asma Ahmadzai
- * Student Number: 101333739
- * SYSC1006 - Assignment 03
- *************************************************/
-
 #ifndef DATA_STRUCTURES_H
 #define DATA_STRUCTURES_H
 
-/********** DON'T MODIFY **********/
 typedef struct player {
     int id;
     char name[50];
@@ -20,6 +13,5 @@ typedef struct queue {
     player_t *rear;
     int size;
 } queue_t;
-/********** DON'T MODIFY **********/
 
 #endif
